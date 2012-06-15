486d4cf89b7912f927d5391f9889a4255f9f26fe
brunob <brunobergot@gmail.com>
2012-06-15 15:57:35 +0000

    conversion vers paquet avec plugonet, merci tonton :) + maj des necessites

---
+++ lang/paquet-geol_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-geol
+// Langue: fr
+// Date: 15-06-2012 17:42:39
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// G
+	'geol_description' => 'Squelettes du projet Géodiversité.net.',
+	'geol_slogan' => 'Squelettes du projet Géodiversité',
+);
+?>
