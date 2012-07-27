5c20b1d3fd09e0291b8a3411fa06f7bc512b3bd9
brunob <brunobergot@gmail.com>
2012-07-27 08:50:03 +0000

    réparer la surcharge du mail d'inscription

--- geol_options.php
+++ geol_options.php
@@ -24,30 +24,4 @@ function autoriser_modererforum($faire, $type, $id, $qui, $opt) {
 	return autoriser_ecrire($faire, $type, $id, $qui, $opt);
 }
 
-// construction du mail envoyant les identifiants 
-// fonction redefinissable qui doit retourner un tableau
-// dont les elements seront les arguments de inc_envoyer_mail
-
-function envoyer_inscription($desc, $nom, $mode, $id) {
-
-	$nom_site_spip = nettoyer_titre_email($GLOBALS['meta']["nom_site"]);
-	$adresse_site = $GLOBALS['meta']["adresse_site"];
-	$adresse_login = $adresse_site;
-	if ($mode == '6forum') {
-		$msg = 'form_forum_voici1';
-	} else {
-		$msg = 'form_forum_voici2';
-	}
-
-	$msg = _T('form_forum_message_auto')."\n\n"
-		. _T('form_forum_bonjour', array('nom'=>$nom))."\n\n"
-		. _T($msg, array('nom_site_spip' => $nom_site_spip,
-			'adresse_site' => $adresse_site . '/',
-			'adresse_login' => $adresse_login . '/')) . "\n\n- "
-		. _T('form_forum_login')." " . $desc['login'] . "\n- "
-		. _T('form_forum_pass'). " " . $desc['pass'] . "\n\n";
-
-	return array("[$nom_site_spip] "._T('form_forum_identifiants'), $msg);
-}
-
 ?>
