9bb2259133a76a2868dd0f03a840c5fa5a3332fc
brunob <brunobergot@gmail.com>
2012-08-04 14:39:05 +0000

    faux placeholder à la chrysalide pour le formulaire de recherche

--- javascript/geol.js
+++ javascript/geol.js
@@ -1,10 +1,16 @@
 (function($){
 	$(function(){
-		$('#formulaire_recherche #recherche').val('RECHERCHER').click(function(){
-			if ($(this).val() == 'RECHERCHER'){
-				$(this).val('');
-			}
-		});
+		$('#formulaire_recherche label').css({'position':'absolute','left':'5px','top':'0'});
+		if ($('input#recherche').val().length == 0) {
+			$('input#recherche').focusin(function(){
+				$('#formulaire_recherche label').fadeOut(1000);
+			});
+			$('input#recherche').focusout(function(){
+				$('#formulaire_recherche label').fadeIn(1000);
+			});
+		} else {
+			$('#formulaire_recherche label').hide();
+		}
 
 		// Menu depliant de navigation de programmer.spip.org
 		
