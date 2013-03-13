845046409acd33f5caef7395dbb0340e38f47788
brunob <brunobergot@gmail.com>
2013-03-13 16:53:27 +0100

    ces chaînes sont dispos dans media_collections

--- geodiversite_complements/geodiversite_albums/lang/geol_albums_fr.php
+++ geodiversite_complements/geodiversite_albums/lang/geol_albums_fr.php
@@ -11,8 +11,6 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 
 'explication_ouvrir' => 'Vous pouvez ouvrir cette collection à la coopération afin d\'autoriser les autres auteurs à y ajouter des médias.',
-'explication_participer' => 'Cliquez sur le bouton ci-dessous pour participer à cette collection.',
-'explication_quitter' => 'Cliquez sur le bouton ci-dessous pour ne plus participer àà cette collection.',
 'explication_suivi' => 'Suivre les contributions de la collection via le fil RSS. Exporter la carte de ses contributions vers un autre outil de cartographie.',
 'explication_supprimer' => 'Supprimer cette collection.',
 'explication_supprimer_confirm' => 'Êtes-vous sûr de vouloir supprimer cette collection ? (le contenu de la collection ne sera pas supprimé)',
