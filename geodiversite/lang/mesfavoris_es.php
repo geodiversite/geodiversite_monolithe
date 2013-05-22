230a329b46be6e3c71fae0bec7dc6ef441b10e70
kent1 <kent1@arscenic.info>
2013-05-22 15:51:07 +0200

    La totale en espagnol

---
+++ geodiversite/lang/mesfavoris_es.php
@@ -0,0 +1,14 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+'add' => 'Añadir a mis favoritos',
+'add_to' => 'Añadir a mis favoritos',
+'remove' => 'Quitar de mis favoritos',
+'remove_from' => 'Quitar de mis favoritos'
+
+);
+
+?>
---
+++ geodiversite/lang/xmlrpc_geodiv_es.php
@@ -0,0 +1,11 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	// E
+	'erreur_article_media' => 'Este artículo no parece ser un documento media (#@id_article@)'
+
+);
+
+?>
