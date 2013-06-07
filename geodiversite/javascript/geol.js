cd741aa80166ebbe954a326d6a6ebee8f1dacb08
brunob <brunobergot@gmail.com>
2013-06-07 11:00:13 +0200

    - pas de classe menu sur le bloc embed_code par défaut + ajout d'un apramètre class pour permette d'ajouter une class à ce bloc - passer le bloc embed_code dans le .menu.suivi sur la page auteur

--- geodiversite/javascript/geol.js
+++ geodiversite/javascript/geol.js
@@ -50,14 +50,14 @@
 		
 		// Bloc dépliable pour code_embed, pompé sur mediaspip http://zone.spip.org/trac/spip-zone/browser/_squelettes_/mediaspip/mediaspip_core/trunk/javascript/mediaspip_base.js#L9
 		$('.code_embed').each(function(){
-			var parent = $(this).parents('div.menu.embed');
+			var parent = $(this).parents('div.embed');
 			if(parent.find('span.title').size() > 0){
 				var id_code = parent.find('textarea').attr('id');
 				parent.find('.formulaire_embed_code').hide();
 				parent.find('span.title').append('<span class="show_code show_code_plus">&nbsp;+</span>').wrapInner('<a class="show_code" href="#'+id_code+'"></a></span>');
 			}
 			$('.show_code').click(function(){
-				var parent = $(this).parents('div.menu.embed');
+				var parent = $(this).parents('div.embed');
 				if(parent.find('.formulaire_embed_code').is(':hidden')){
 					parent.find('.formulaire_embed_code').slideDown("normal");
 					parent.find('span.show_code').html('&nbsp;-').removeClass('show_code_plus').addClass('show_code_moins');
