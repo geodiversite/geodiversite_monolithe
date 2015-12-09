1c6ab34be6bc53bda3588b9de42da2f888482e02
brunob <brunobergot@gmail.com>
2015-12-09 12:00:33 +0100

    suivre memoization

--- install_geodiversite.sh
+++ install_geodiversite.sh
@@ -51,7 +51,7 @@ svn co svn://zone.spip.org/spip-zone/_plugins_/licence
 svn co svn://zone.spip.org/spip-zone/_plugins_/mailshot
 svn co svn://zone.spip.org/spip-zone/_plugins_/mailsubscribers/trunk mailsubscribers
 svn co svn://zone.spip.org/spip-zone/_plugins_/emballe_medias/media_collections/trunk media_collections
-svn co svn://zone.spip.org/spip-zone/_plugins_/memoization
+svn co svn://zone.spip.org/spip-zone/_plugins_/memoization/trunk memoization
 svn co svn://zone.spip.org/spip-zone/_plugins_/twitter/trunk twitter
 svn co svn://zone.spip.org/spip-zone/_plugins_/minibando/trunk minibando
 svn co svn://zone.spip.org/spip-zone/_plugins_/newsletters
