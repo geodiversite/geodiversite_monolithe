f66e4680129622d11287a90f45a0d1d1b3841662
brunob <brunobergot@gmail.com>
2018-08-14 20:35:56 +0200

    page carte : réparer l'option openInfowindow pour les points affichés dans un clustergroup
    cf https://github.com/Leaflet/Leaflet.markercluster/issues/647

--- geodiversite/javascript/leaflet.geodiv.js
+++ geodiversite/javascript/leaflet.geodiv.js
@@ -173,10 +173,11 @@ L.Map.Geodiv = L.Map.extend({
 		});
 		// lors de l'ajout d'un marker au cluster, vérifier si on doit ouvrir sa popup (uniquement au chargement)
 		if (me.options.openInfowindow) {
-			// brancher layeradd sur markerCluster._featureGroup, cf https://github.com/Leaflet/Leaflet.markercluster/issues/368
-			me.markerCluster._featureGroup.on('layeradd', function (e) {
-				if (e.layer.id==me.options.openInfowindow)
+			var openInfowindowId = me.options.openInfowindow;
+			me.markerCluster.on('layeradd', function (e) {
+				if (e.layer.id == openInfowindowId) {
 					me.getInfowindow(e.layer.id,e.layer.getLatLng());
+				}
 				me.options.openInfowindow = false;
 			});
 		}
